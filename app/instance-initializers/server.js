6fec20e7384c4208c2d343aaf57363ec2e949694
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-08-10 14:04:10 +0100

    add instance initializer

---
+++ app/instance-initializers/server.js
@@ -0,0 +1,9 @@
+import server from '../utils/server';
+
+export function initialize(/* appInstance */) {
+  // appInstance.inject('route', 'foo', 'service:foo');
+}
+
+export default {
+  initialize
+};
